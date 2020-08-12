6ec349a34630f75a5fd453defdd15a8b85df365d
danie <daniel.freitas.ms1@gmail.com>
2020-08-12 11:39:36 -0300

    Primeiro Commit

---
+++ src/main/java/com/br/thundersJava/ThundersJavaApplication.java
@@ -0,0 +1,13 @@
+package com.br.thundersJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThundersJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThundersJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/thundersJava/ThundersJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.thundersJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThundersJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
