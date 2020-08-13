e56126a6e66a7cc97bfd2fb4b4ad240e97a1cec0
DANIEL DE FREITAS FONSECA <987312845@itaud.des.ihf>
2020-08-13 11:12:20 -0300

    comentado a linha de email

--- src/main/java/com/br/thundersJava/services/SendMailService.java
+++ src/main/java/com/br/thundersJava/services/SendMailService.java
@@ -1,25 +1,25 @@
-package com.br.thundersJava.services;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class SendMailService {
-	@Autowired 
-	private JavaMailSender mailSender;
-	  
-	public void sendEmail (String sendTo) {
-		
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setSubject("Cadastro Realizado com Sucesso!");
-        message.setText("Testado");
-        message.setTo(sendTo);
-        message.setFrom("thunderjava2020@gmail.com");
-        
-        mailSender.send(message);
-        System.out.println("EMAIL ENVIADO COM SUCESSO PARA: " + sendTo);
-        
-	}
-}
+//package com.br.thundersJava.services;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class SendMailService {
+//	@Autowired 
+//	private JavaMailSender mailSender;
+//	  
+//	public void sendEmail (String sendTo) {
+//		
+//		SimpleMailMessage message = new SimpleMailMessage();
+//		message.setSubject("Cadastro Realizado com Sucesso!");
+//        message.setText("Testado");
+//        message.setTo(sendTo);
+//        message.setFrom("thunderjava2020@gmail.com");
+//        
+//        mailSender.send(message);
+//        System.out.println("EMAIL ENVIADO COM SUCESSO PARA: " + sendTo);
+//        
+//	}
+//}
