8cde941557c6e494cdde2fa50b0c73e3beb3752e
ADRIANA DOS SANTOS <007390198@itaud.des.ihf>
2020-08-13 13:06:20 -0300

    novos controllers

---
+++ src/main/java/com/br/thundersJava/controller/RelatorioController.java
@@ -0,0 +1,5 @@
+package com.br.thundersJava.controller;
+
+public class RelatorioController {
+
+}
--- src/main/java/com/br/thundersJava/services/SendMailService.java
+++ src/main/java/com/br/thundersJava/services/SendMailService.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-/*package com.br.thundersJava.services;
+/*//package com.br.thundersJava.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -24,30 +23,3 @@ public class SendMailService {
         
 	}
 }*/
-=======
-//package com.br.thundersJava.services;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class SendMailService {
-//	@Autowired 
-//	private JavaMailSender mailSender;
-//	  
-//	public void sendEmail (String sendTo) {
-//		
-//		SimpleMailMessage message = new SimpleMailMessage();
-//		message.setSubject("Cadastro Realizado com Sucesso!");
-//        message.setText("Testado");
-//        message.setTo(sendTo);
-//        message.setFrom("thunderjava2020@gmail.com");
-//        
-//        mailSender.send(message);
-//        System.out.println("EMAIL ENVIADO COM SUCESSO PARA: " + sendTo);
-//        
-//	}
-//}
->>>>>>> e56126a6e66a7cc97bfd2fb4b4ad240e97a1cec0
