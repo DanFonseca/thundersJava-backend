7603974cd9e4e374c0c32c7364d264c5898f14c0
danie <daniel.freitas.ms1@gmail.com>
2020-08-14 10:39:58 -0300

    simple date format para email service

--- src/main/java/com/br/thundersJava/services/EmailServiceImpl.java
+++ src/main/java/com/br/thundersJava/services/EmailServiceImpl.java
@@ -1,5 +1,7 @@
 package com.br.thundersJava.services;
 
+import java.text.SimpleDateFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -14,12 +16,13 @@ public class EmailServiceImpl {
 	    private JavaMailSender emailSender;
 
 	    public void sendSimpleMessage(Agendamento agendamento) {
-
+	    	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	    	
 	        SimpleMailMessage message = new SimpleMailMessage(); 
 	        message.setFrom("thunderjava2020@gmail.com");
 	        message.setTo(agendamento.getEmailCliente()); 
 	        message.setSubject("Cadastro Realizado com Sucessp!"); 
-	        message.setText("Prezado, " + agendamento.getNomeCliente() + "Informamos que seu agendamento foi cadastrado com sucesso! Para o dia " + agendamento.getData() + "'as " + agendamento.getHora());
+	        message.setText("Prezado " + agendamento.getNomeCliente() + ", Informamos que seu agendamento foi cadastrado com sucesso! Para o dia " + sdf.format(agendamento.getData()) + " as " + agendamento.getHora());
 	        emailSender.send(message);
 	    }
 
